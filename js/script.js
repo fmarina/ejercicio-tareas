43c5e1ecc8543ddef0fc53aeaac8c5daceff3986
Marina <marinafaenze@hotmail.com>
2019-11-09 18:36:31 -0300

    proyecto tareas listo para subit a git

--- js/script.js
+++ js/script.js
@@ -1,56 +1,78 @@
-//para crear las tareas necesitamos primero un lugar a donde guardarlas.
-//tenemos que crear un variable del tipo array.
-//esta array va a guardar otros arrays, con la estructura que se muestra.
+var task = [];
 var tareas = [
   ['tarea 1', false],
   ['tarea 2', false]
 ];
 
-//* * Funcion agregar tarea:
-//* * tenemos que preguntarle al usuario repetidamente hasta que haga click en "cancelar";
-//lo ideal para esto es utilizar un prompt.
-
-//* * la pregunta debe ser guardada en una variable que será agregada al array tareas creado anteriormente
-//* * pero antes de agregarla deberíamos verificar que el usuario no haya dejado vació el campo de la prompt. Entonces, si existe la tarea, continuamos.
-
-//* * además de verificar la existencia de la tarea, tendríamos que verificar que ya no exista una tarea con el mismo nombre.
-
-//* * para ello podríamos crear una función, por ejemplo: verificarExistencia, que recibirá como parámetro el nombre de la tarea que el usuario quiere agregar.
-//* * esta función debe devolver true si existe la tarea, o false  si la tarea no existe.
-
-//* * en el primer caso (true), le daremos al usuario una alerta que le diga que la tarea ya existe.
-//* * en el caso de false, "empujaremos" la nueva tarea al array de tareas, debe tener el siguiente formato: [tarea, false];
-
-//* * ponemos false como segundo elmento en el array de tarea, porque inicialmente la tarea no está realizada
-
-//* * además debemos llamar a la función createTareaElement() que recibe como parámetro la tarea. Esta función que ya está dada, tiene por fin crear el elemento de la lista que verá el usuario en el front end.
-
-
-
-
-// * * debemos crear una función que deberá marcar una tarea cómo realizada, tanto en el array tareas, como en el front end.
-// * * lo primero que tenemos que hacer es:
-
-// * * preguntarle al usuario cuál es la tarea que quiere marcar como finalizada o realizada. La guardamos en una variable.
-// * * luego necesitamos dos funciones, una para arreglar el array de tareas y cambiar el valor de la tarea correspondiente con true. 
-// * * Esta tarea, que se podría llamar buscarTareaEnArray(), recibirá cómo parámetro la tarea que el usuario quiere marcar como finalizada. 
-// * * Tendrá que recorrer todo el arreglo tareas, y buscar entre las tareas, que también son arreglos, aquella cuyo elemento del índice 0, sea igual al nombre de la tarea para marcar.
-//* * se puede usar un for o un forEach.
-
-// * * La otra función ya está lista, se llama marcarTareaEnFrontEnd() y recibe como parámetro el nombre de la tarea a marcar, sólo deben llamarla (ejecutarla).
-
-function marcarComoRealizada() {
-  //función para buscar tarea en el array;
-  //llamar a la función para marcar la tarea en el front end.
- 
+function agregarTarea(){
+  var tarea
+  do {
+    tarea = prompt("Ingrese la tarea que desea agregar");
+    if(tarea != "" && tarea != null){
+      if(verificarExistencia(tarea)){
+        alert(`La tarea ${tarea} ya existe.`);      
+      }
+      else{
+        task.push(tarea, false);
+        tareas.push(task);
+        task = [];
+        createTareaElement(tarea);
+      }           
+    }
+    else{
+      return;
+    }    
+  } while (tarea != null);  
 }
 
-// * * crear una función para contar todas las tareas pendientes, se tiene que llamar: contarPendientes()
+function verificarExistencia(tarea){
+  for(var i = 0; i < tareas.length; i++){
+    if(tareas[i][0] == tarea){
+      return true;
+    }
+  }
+  return false;
+}  
 
-// * *  crear una función para contar todas las tareas realizadas, se tiene que llamar: contarFinalizadas()
+function marcarComoRealizada() {
+  var realizada = prompt("Que tarea desea marcar como realizada?"); 
+  buscarTareaEnArray(realizada);
+  marcarTareaEnFrontEnd(realizada);  
+}
 
+function buscarTareaEnArray(tarea){ 
+  tareas.forEach(function(e){
+    e.forEach(function(element, i){
+      if(element == tarea){
+        e[i+1] = true;
+      }
+    });
+  });    
+}
 
-//funciones listas.
+function contarPendientes(){
+  var tareasPendientes = 0;
+  tareas.forEach(function(e){
+    e.forEach(function(element){
+      if(element == false){
+        tareasPendientes++;
+      }
+    });
+  });
+  return alert("Tareas pendientes: " + tareasPendientes);
+}
+function contarFinalizadas(){
+  var tareasRealizadas = 0;
+  tareas.forEach(function(e){
+    e.forEach(function(element){
+      if(element == true){
+        tareasRealizadas++;
+      }
+    });
+  });
+  return alert("Tareas realizadas: " + tareasRealizadas);
+}
+
 
 var tareasContainer = document.querySelector('#tareas');
 var tareasList = document.createElement('ol');
